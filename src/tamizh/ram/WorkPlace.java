2a0f661e2d6b5b050576c1fad5ef55bfe19cd88b
Jayakumar <jayakumar2242@gmail.com>
2022-06-14 13:24:35 +0530

    new project

---
+++ src/tamizh/ram/WorkPlace.java
@@ -0,0 +1,21 @@
+package tamizh.ram;
+
+public class WorkPlace {
+	public void workchennai() {
+		System.out.println("CENNAI BEACH STATION");
+	}
+	public void hospitalname() {
+		System.out.println("CHENNAI NATIONAL HOSPITAL");
+	}
+	public static void main(String[] args) {
+		WorkPlace W = new WorkPlace();
+		W.workchennai();
+		W.hospitalname();
+		WorkingCnh P = new WorkingCnh();
+		P.workexperience();
+		P.salary();
+		
+	}
+	}
+
+
---
+++ src/tamizh/ram/WorkingCnh.java
@@ -0,0 +1,21 @@
+package tamizh.ram;
+
+public class WorkingCnh {
+	public void workexperience() {
+		System.out.println("3yeasrs");
+		
+	}
+	public void salary() {
+		System.out.println("13000");
+		
+	}
+	public static void main(String[] args) {
+		WorkingCnh P = new WorkingCnh();
+		P.workexperience();
+		P.salary();
+		
+				
+	}
+	
+
+}
